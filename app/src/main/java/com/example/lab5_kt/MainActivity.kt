d86ca7355b11ace99d1f5341901101b12f436f4c
Sivan Chen <ryan1825114@gmail.com>
2024-06-13 10:55:19 +0800

    set background and animation about 8 seconds to change transparency

--- app/src/main/java/com/example/lab5_kt/MainActivity.kt
+++ app/src/main/java/com/example/lab5_kt/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lab5_kt
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener { executeAnimation1() }
     }
+
+    private fun executeAnimation1() {
+        val animation1 = AnimationUtils.loadAnimation(this, R.anim.animation1)
+        val imageView: ImageView = findViewById(R.id.imageView)
+        imageView.startAnimation(animation1)
+        val imageView2: ImageView = findViewById(R.id.imageView2)
+        imageView2.startAnimation(animation1)
+    }
+
+
 }
